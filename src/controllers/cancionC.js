d9cfc0ea3dd384710eba4c11f4c75907d339c0c2
bsquintana55 <bsquintana55@misena.edu.co>
2022-12-06 23:11:14 -0500

    canción post

--- src/controllers/cancionC.js
+++ src/controllers/cancionC.js
@@ -7,10 +7,9 @@ exports.obtener = async (req, res) => {
     const cancion = await Cancion.find().populate('album',{
 
       "_id":1,
-      "nombreCancion": 1,
-      "fechaGrabacion": 1,
-      "duracionCancion": 1,
-      "estadoCancion": 1
+      "nombreAlbum": 1,
+      "anioPublicacion": 1,
+      "estadoAlbum": 1
     });;
     res.status(200).json(cancion);
   } catch (error) {
@@ -24,11 +23,10 @@ exports.obtenerid = async (req, res) => {
       const id = req.params.id;
       const cancion = await Cancion.findById(id).populate('album',{
 
-      "_id":1,
-      "nombreCancion": 1,
-      "fechaGrabacion": 1,
-      "duracionCancion": 1,
-      "estadoCancion": 1
+        "_id":1,
+        "nombreAlbum": 1,
+        "anioPublicacion": 1,
+        "estadoAlbum": 1
 
       });;
       res.status(200).json(cancion);
@@ -41,60 +39,26 @@ exports.obtenerid = async (req, res) => {
   //registrar
   exports.add = async (req, res) => {
     try {
-  //crea constantes que suspongo que son para utilizarlas y mostralas en la vista
-      const {
-        _id,
-        nombreCancion,
-        fechaGrabacion,
-        duracionCancion,
-        estadoCancion,
-        albumId
-
-     } = new req.body;
-     //console.log(req.file);
-
-
-     //tae desde el modelo los id y hace una consulta por id
-     const album=await Album.findById(albumId);
-     console.log(album._id);
-
-
-     const nCancion = new Cancion({
-      _id,
-      nombreCancion,
-      fechaGrabacion,
-      duracionCancion,
-      estadoCancion,
-      albumId: album._id    
-       
-     })
      
-     try{
-       const saveCancion=await nCancion.save();
-       album.cancion=album.cancion.concat(saveCancion._id);
-       await album.save();
-
-       console.log(saveCancion)
-       res.status(200).json(saveCancion);
-       //segundo try
-     }catch (error) {
-       res.status(500).json({msj:"Error al registrar"+error})
-     }
-//catch del primer try
-   } catch (error) {
-      res.status(500).json({msj:"Error al registrar"+error})
+      const nCancion = new Cancion(req.body,req.file)
+      console.log(req.file);
+      await nCancion.save();
+      console.log(nCancion);
+      res.json({ msj: "la cancion se registro exitosamente ", id: nCancion._id })
+    } catch (error) {
+      res.status(500).json({msj:"Error al registrar "+error})
     }
   
   }
 
 exports.edit = async(req, res) => {
     try {
-      const _id = req.params._id;
+      const id = req.params.id;
       const nCancion = new Cancion(req.body,req.file)
       console.log(req.file);
 
       
-      const cambioCancion = await cancion.findByIdAndUpdate(_id, nCancion);
+      const cambioCancion = await cancion.findByIdAndUpdate(id, nCancion);
       res.json({ msj: "la cancion fue actualizada exitosamente"})
     } catch(error) {
       res.status(500).json(error);
